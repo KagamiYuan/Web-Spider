b1746ee463c9965bd15be418443859c005dbab32
Kagami Yuan <yuany.kagami@gmail.com>
2014-10-30 09:32:25 +0800

    爬集佳
    http://www.unitalenlaw.com/html/unitalenlaw/category/46559-1.htm?&l6930=
    0
    尝试爬集佳的文章

---
+++ paper downloader.R
@@ -0,0 +1,15 @@
+setwd("/Users/yuanyuan/快盘/R/Web Spider")
+
+
+library(XML)
+for(i in 14000000:15000000){
+        paper.url <- paste("http://www.unitalenlaw.com/html/unitalenlaw/report/",i,"-1.htm",sep="")
+        paper.html <- htmlTreeParse(paper.url, useInternalNodes=T)#读取html
+        article <- xpathSApply(paper.html, "//p", xmlValue)#读取<p></p>之间的内容
+        title <- xpathSApply(paper.html,"//title", xmlValue)#读取title
+        article[1] <- title
+        write(article, file=paste(i,"-",title, ".txt", sep=""), sep=" ")#写成txt
+}
+
+
+
